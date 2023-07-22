b09c7c037413e36f14c87d29b04b8b8c45202939
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-07-22 11:42:55 +0200

    Display tracker stats

--- js/app.js
+++ js/app.js
@@ -4,14 +4,67 @@ class CalorieTracker {
   #meals = [];
   #workouts = [];
 
+  constructor() {
+    this.#displayCaloriesLimit();
+    this.#displayCaloriesTotal();
+    this.#displayCaloriesConsumed();
+    this.#displayCaloriesBurned();
+    this.#displayCaloriesRemaining();
+  }
+
+  // Public Methods
   addMeal(meal) {
     this.#meals.push(meal);
     this.#totalCalories += meal.calories;
+    this.#render();
   }
 
   addWorkout(workout) {
     this.#workouts.push(workout);
     this.#totalCalories -= workout.calories;
+    this.#render();
+  }
+
+  // Private Methods
+  #displayCaloriesTotal() {
+    const totalCaloriesEl = document.getElementById('calories-total');
+    totalCaloriesEl.innerHTML = this.#totalCalories;
+  }
+
+  #displayCaloriesLimit() {
+    const calorieLimitEl = document.getElementById('calories-limit');
+    calorieLimitEl.innerHTML = this.#calorieLimit;
+  }
+
+  #displayCaloriesConsumed() {
+    const caloriesConsumedEl = document.getElementById('calories-consumed');
+    // prettier-ignore
+    const consumed = this.#meals.reduce((total, meal) => total + meal.calories,0);
+
+    caloriesConsumedEl.innerHTML = consumed;
+  }
+
+  #displayCaloriesBurned() {
+    const caloriesBurnedEl = document.getElementById('calories-burned');
+    // prettier-ignore
+    const burned = this.#workouts.reduce((total, workout) => total + workout.calories,0);
+
+    caloriesBurnedEl.innerHTML = burned;
+  }
+
+  #displayCaloriesRemaining() {
+    const caloriesRemainingEl = document.getElementById('calories-remaining');
+
+    const remaining = this.#calorieLimit - this.#totalCalories;
+
+    caloriesRemainingEl.innerHTML = remaining;
+  }
+
+  #render() {
+    this.#displayCaloriesTotal();
+    this.#displayCaloriesConsumed();
+    this.#displayCaloriesBurned();
+    this.#displayCaloriesRemaining();
   }
 }
 
@@ -34,9 +87,13 @@ class Workout {
 const tracker = new CalorieTracker();
 
 const breakfast = new Meal('Breakfast', 400);
+const lunch = new Meal('Lunch', 350);
 tracker.addMeal(breakfast);
+tracker.addMeal(lunch);
 
 const run = new Workout('Run', 300);
+const run2 = new Workout('Run', 300);
 tracker.addWorkout(run);
+tracker.addWorkout(run2);
 
 console.log(tracker);
